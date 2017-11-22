6a7b52083051aa6a9aa0e090bd7ca98501dd2087
jakezenger <jakezenger@hotmail.com>
2017-11-21 19:10:39 -0800

    Added GetCellID method to GameModel

--- MobileTag/Models/GameModel.cs
+++ MobileTag/Models/GameModel.cs
@@ -9,12 +9,37 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Drawing;
 
 namespace MobileTag.Models
 {
     public class GameModel : Application
     {
+        // IMPORTANT: corresponds to Frontier's specifications.
+        // Do not change this unless you first recreate the Frontier's cell database using your new specifications.
+        private const decimal frontierInterval = .00009375m;
+        private const decimal frontierUpperLeftLat = 44.677684m;
+        private const decimal frontierUpperLeftLong = 57.232579m;
+        private const decimal frontierLowerRightLat = 44.679184m;
+        private const decimal frontierLowerRightLong = 57.234079m;
+
+        public decimal FrontierInterval => frontierInterval;
+
         //public List<Cell> CellsInView { get; set; } -- SHOULD MAYBE GO IN MAP ACTIVITY... DOES THIS NEED TO BE HERE?
         public Player Player { get; set; }
+
+
+        public int GetCellID(decimal lat, decimal lng)
+        {
+            decimal nearestLatInterval, nearestLongInterval;
+
+            nearestLatInterval = (Math.Floor((lat - frontierUpperLeftLat) / frontierInterval) * frontierInterval);
+            nearestLongInterval = (Math.Floor((lng - frontierUpperLeftLong) / frontierInterval) * frontierInterval);
+
+            int id = Convert.ToInt32((nearestLongInterval / frontierInterval) + (nearestLatInterval / frontierInterval) 
+                * ((frontierLowerRightLong - frontierUpperLeftLong) / frontierInterval));
+
+            return id;
+        }
     }
 }
