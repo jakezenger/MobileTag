5fa1104b110e0e92336b8cbcc156cc86ad1c7f6d
nick pettit <nickppettit@gmail.com>
2017-11-25 09:21:53 -0800

    End of first term test commit - all map features (location, overlay)

--- MobileTag/MainActivity.cs
+++ MobileTag/MainActivity.cs
@@ -1,18 +1,31 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using System;
 
 namespace MobileTag
 {
     [Activity(Label = "MobileTag")]
     public class MainActivity : Activity
     {
+        Button mButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+            mButton = FindViewById<Button>(Resource.Id.btnToMapTest);
+            mButton.Click += mButton_Click;
+        }
+
+        void mButton_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(MapActivity));
+            this.StartActivity(intent);
+
         }
     }
 }
