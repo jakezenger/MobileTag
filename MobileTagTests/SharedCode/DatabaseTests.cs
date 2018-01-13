b0b352b0a5546d68aad2163315a9fa502f22f5b0
Tanel Helmik <vigilantmaster@hotmail.com>
2018-01-12 18:20:27 -0800

    Added Unit Tests

---
+++ MobileTagTests/SharedCode/DatabaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MobileTag;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileTag.Tests
+{
+    [TestClass()]
+    public class DatabaseTests
+    {
+        [TestMethod()]
+        public void GetCellTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
