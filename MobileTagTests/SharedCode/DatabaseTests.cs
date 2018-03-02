3876afad4aaa326a0376ae4114dac8732759b259
jakezenger <jakezenger@gmail.com>
2018-03-02 12:15:38 -0800

    Updated Tanel's test

--- MobileTagTests/SharedCode/DatabaseTests.cs
+++ MobileTagTests/SharedCode/DatabaseTests.cs
@@ -2,21 +2,42 @@
 using MobileTag;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MobileTag.Models;
 
+
 namespace MobileTag.Tests
 {
+
+    //[TestClass]
+    //public void Save_CustomerIsNotNull_GetsAddedToRepository()
+    //{
+    ////Arrange
+    //Mock<IContainer> mockContainer = new Mock<IContainer>();
+    //Mock<ICustomerView> mockView = new Mock<ICustomerView>();
+
+    //CustomerViewModel viewModel = new CustomerViewModel(mockView.Object, mockContainer.Object);
+    //viewModel.CustomersRepository = new CustomersRepository();
+    //viewModel.Customer = new Mock<Customer>().Object;
+
+    ////Act
+    //viewModel.Save();
+
+    ////Assert
+    //Assert.IsTrue(viewModel.CustomersRepository.Count == 1);
+    //}
+
+
     [TestClass()]
     public class DatabaseTests
     {
-        public Cell TestCell;
         [TestInitialize]
         public void _init()
         {
-            
+
         }
 
         [TestMethod()]
@@ -24,35 +45,29 @@ namespace MobileTag.Tests
         {
             Cell comparisonCell = new Cell(0);
             comparisonCell.Latitude = 44.677684000000000m;
-             comparisonCell.Longitude =  57.23257900000000m;
-            TestCell = Database.GetCell(0);
-          
-            Assert.AreEqual(comparisonCell.Latitude,TestCell.Latitude);
+            comparisonCell.Longitude = 57.23257900000000m;
+            Cell TestCell = Database.GetCell(0);
+
+            Assert.AreEqual(comparisonCell.Latitude, TestCell.Latitude);
             Assert.AreEqual(comparisonCell.Longitude, TestCell.Longitude);
-        }
-
-        [TestMethod()]
-        public void AddUserTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void ValidateLoginCredentialsTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void GetCellTeamTest()
-        {
-            Assert.Fail();
         }
 
         [TestMethod()]
         public void GetPlayerTest()
         {
-            Assert.Fail();
+            Team team = new Team(2, "Green");
+            Player testPlayer = new Player(1, "ethan", team, 0);
+            Player fromServer = Database.GetPlayer("ethan");
+
+            Assert.AreEqual(testPlayer.ID, fromServer.ID);
+            Assert.AreEqual(testPlayer.Team.ID, fromServer.Team.ID);
+            Assert.AreEqual(testPlayer.Team.TeamName, fromServer.Team.TeamName);
+        }
+
+        [TestMethod()]
+        public void ValidateLoginCredentialsTest1()
+        {
+            Assert.AreEqual(1, Database.ValidateLoginCredentials("nicknick", "nicknick"));
         }
     }
 }
