7e92e5f9f7860a120046a9d3911ca55bff785f46
jakezenger <jakezenger@gmail.com>
2018-03-09 16:32:32 -0800

    Revert "updated frontier"
    This reverts commit 28a7ac451ecb30518b183c95b8c1da958cfcb924.

--- MobileTag/MapActivity.cs
+++ MobileTag/MapActivity.cs
@@ -24,7 +24,7 @@ namespace MobileTag
     [Activity(Label = "MapActivity")]
     public class MapActivity : Activity, IOnMapReadyCallback, Android.Locations.ILocationListener, GoogleMap.IOnCameraIdleListener
     {
-        private const double CELL_LOAD_RADIUS = .0010;
+        private const double CELL_LOAD_RADIUS = .0006;
         private const int ZOOM_LEVEL_LOAD = 15;
         private GoogleMap mMap;
         private float currentZoomLevel = 0.0F;
