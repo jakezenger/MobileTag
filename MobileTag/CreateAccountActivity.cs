5b4001e4303766cb8e6e620d5b447e97ec53a106
jakezenger <jakezenger@gmail.com>
2018-03-13 15:29:19 -0700

    logging out now works

--- MobileTag/CreateAccountActivity.cs
+++ MobileTag/CreateAccountActivity.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace MobileTag
 {
-    [Activity(Label = "Create Account")]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
     public class CreateAccountActivity : Activity
     {
         private bool validTeamSelected = false;
@@ -26,6 +26,9 @@ namespace MobileTag
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.CreateAccount);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
 
             string[] teams = { GetString(Resource.String.select_team_prompt), "Red", "Green", "Blue", "Purple", "Pink" };
 
