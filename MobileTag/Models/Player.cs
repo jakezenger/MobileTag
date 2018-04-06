e7e219ef987842a11452c065cdf95c660a29baea
Nicholas Pettit <nicholas.pettit@oit.edu>
2018-04-05 20:22:20 -0700

    :Added a wallet class that holds Confinium "Player Points" :Added new wallets to player constructor :Added new  wallet databaseTest :Added wallet to GetPlayer - Currently doesnt contact database.

--- MobileTag/Models/Player.cs
+++ MobileTag/Models/Player.cs
@@ -18,23 +18,26 @@ namespace MobileTag.Models
         public int ID { get; }
         public Team Team { get; set; }
         public int CurrentCellID { get; set; }
+        public Wallet Wallet { get; set; }
         public List<Mine> Mines { get; }
 
-        public Player(int id, string username, Team team, int currentCellID, List<Mine> mines)
+        public Player(int id, string username, Team team, int currentCellID, List<Mine> mines, Wallet wallet)
         {
             ID = id;
             Team = team;
             CurrentCellID = currentCellID;
             Username = username;
+            Wallet = wallet;
             Mines = mines;
         }
 
-        public Player(int id, string username, Team team, decimal lat, decimal lng, List<Mine> mines)
+        public Player(int id, string username, Team team, decimal lat, decimal lng, List<Mine> mines, Wallet wallet)
         {
             ID = id;
             Team = team;
             CurrentCellID = Cell.FindID(lat, lng);
             Username = username;
+            Wallet = wallet;
             Mines = mines;
         }
 
---
+++ MobileTag/Models/Wallet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MobileTag.Models
+{
+    public class Wallet
+    {
+        public int Confinium { get; set; }
+
+        public void AddConfinium(int amountToAdd)
+        {
+            Confinium = Confinium + amountToAdd;
+        }
+
+        public bool SubtractConfinium(int amountToSubtract)
+        {
+            if((Confinium - amountToSubtract) > 0)
+            {
+                Confinium = Confinium - amountToSubtract;
+                return true;
+            }
+            return false;
+        }
+    }
+}
