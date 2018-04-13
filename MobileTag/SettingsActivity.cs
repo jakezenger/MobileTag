3a4283bd399f7dc5d0342884e7593fc52b7577ee
Tanel Helmik <vigilantmaster@hotmail.com>
2018-04-12 17:31:52 -0700

    Added a checkbox check

--- MobileTag/SettingsActivity.cs
+++ MobileTag/SettingsActivity.cs
@@ -21,11 +21,14 @@ namespace MobileTag
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Settings);
-           
+            CheckBox checkbox = FindViewById<CheckBox>(Resource.Id.CheckBox_MapStyleLight);
             Button SaveButton = FindViewById<Button>(Resource.Id.Save_Button);
             SaveButton.Click += SaveButton_Click;
             // todo: make this checkbox do something other than display it was selected
-           
+            if (GameModel.MapStyle == Resource.Raw.StandardTheme)
+                checkbox.Checked = true;
+            else
+                checkbox.Checked = false;
             
           
             
@@ -35,6 +38,7 @@ namespace MobileTag
         {
             Toast.MakeText(this, "TestSave", ToastLength.Long).Show();
             CheckBox checkbox = FindViewById<CheckBox>(Resource.Id.CheckBox_MapStyleLight);
+
             GameModel.MapStyle = checkbox.Checked ? Resource.Raw.StandardTheme : Resource.Raw.style_json;
 
             this.Finish();
