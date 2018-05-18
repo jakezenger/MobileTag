464cf4d890d23df63d2e309ab4e3c9e2cfa06844
Julian Kaufman <julian.kaufman@oit.edu>
2018-05-17 17:50:51 -0700

    Issue with logout from non-map activities fixed

--- MobileTag/SettingsActivity.cs
+++ MobileTag/SettingsActivity.cs
@@ -50,7 +50,7 @@ namespace MobileTag
                         break;
                     case Resource.Id.nav_logout:
                         GameModel.Logout();
-                        this.Finish();
+                        this.FinishAffinity();
                         StartActivity(new Intent(this, typeof(LoginActivity)));
                         break;
                     default:
